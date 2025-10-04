2409042a9c6e14968f3c6af899b1a2d0043f5942
Yogeeta <yadhvi.ch@gmail.com>
2025-10-04 19:28:02 +0530

    quack app

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -24,7 +24,7 @@ function LoginPage() {
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
       {/* ---------------LEFT------------- */}
-      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
+      <img src={assets.logo_icon} alt="" className='w-[min(30vw,250px)]' />
 
       {/* ---------------RIGHT----------------- */}
 
